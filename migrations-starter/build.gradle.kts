014e98c7f81d2ab043eb1219c5cc8ed8f9026bae
tyler <tyler@cruftbusters.com>
2023-03-12 09:43:16 -0700

    migrations-starter - set log level to info, add kotest, change package, format crap, delete crap

--- migrations-starter/build.gradle.kts
+++ migrations-starter/build.gradle.kts
@@ -1,37 +1,34 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-val postgres_version : String by project
-val h2_version : String by project
-
 plugins {
-    kotlin("jvm") version "1.8.10"
-    id("io.ktor.plugin") version "2.2.4"
-                id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
+  kotlin("jvm") version "1.8.10"
+  id("io.ktor.plugin") version "2.2.4"
+  id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
 }
 
-group = "com.cruftbusters"
+group = "com.cruftbusters.ktor_starter_v2.migrations_starter"
 version = "0.0.1"
 application {
-    mainClass.set("io.ktor.server.netty.EngineMain")
+  mainClass.set("io.ktor.server.netty.EngineMain")
 
-    val isDevelopment: Boolean = project.ext.has("development")
-    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+  val isDevelopment: Boolean = project.ext.has("development")
+  applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
-    implementation("org.postgresql:postgresql:$postgres_version")
-    implementation("com.h2database:h2:$h2_version")
-    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("io.ktor:ktor-server-config-yaml:$ktor_version")
-    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+  implementation("ch.qos.logback:logback-classic:1.2.11")
+  implementation("com.h2database:h2:2.1.214")
+  implementation("io.ktor:ktor-server-config-yaml:2.2.4")
+  implementation("io.ktor:ktor-server-core-jvm:2.2.4")
+  implementation("io.ktor:ktor-server-netty-jvm:2.2.4")
+  implementation("org.postgresql:postgresql:42.5.1")
+  testImplementation("io.kotest:kotest-runner-junit5-jvm:5.5.5")
+  testImplementation("io.ktor:ktor-server-tests-jvm:2.2.4")
+  testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.8.10")
+}
+
+tasks.withType<Test>().configureEach {
+  useJUnitPlatform()
 }
---
+++ migrations-starter/src/main/kotlin/com/cruftbusters/ktor_starter_v2/migrations_starter/Application.kt
@@ -0,0 +1,10 @@
+package com.cruftbusters.ktor_starter_v2.migrations_starter
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>): Unit =
+  io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused")
+fun Application.module() {
+}
