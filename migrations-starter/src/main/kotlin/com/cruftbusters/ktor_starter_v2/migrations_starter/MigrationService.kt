9f455b24608c7ee5c31862205cb910e7551014a7
tyler <tyler@cruftbusters.com>
2023-03-12 12:48:43 -0700

    migrations-starter - close connections

--- migrations-starter/src/main/kotlin/com/cruftbusters/ktor_starter_v2/migrations_starter/MigrationService.kt
+++ migrations-starter/src/main/kotlin/com/cruftbusters/ktor_starter_v2/migrations_starter/MigrationService.kt
@@ -6,27 +6,28 @@ import java.sql.Connection
 typealias MigrationVersion = Int
 
 class MigrationService(
-  connectionSupplier: () -> Connection,
+  private val connectionSupplier: () -> Connection,
   private val logger: Logger,
   private val name: String = "migration-service",
   private val statements: MigrationStatements,
 ) {
-  private val connection = connectionSupplier().apply {
-    createStatement().execute("create table if not exists migration_sets (name text primary key, version integer)")
-  }
+  fun migrate() = connectionSupplier().use { migrate(it) }
 
-  fun migrate() {
-    val currentVersion = getCurrentVersion()
+  private fun migrate(connection: Connection) {
+    connection.createStatement()
+      .execute("create table if not exists migration_sets (name text primary key, version integer)")
+
+    val currentVersion = getCurrentVersion(connection)
     if (currentVersion != null) {
-      executeMigrations(statements.since(currentVersion))
-      updateVersion(statements.latestVersion)
+      executeMigrations(connection, statements.since(currentVersion))
+      updateVersion(connection, statements.latestVersion)
     } else {
-      executeMigrations(statements.all())
-      insertVersion(statements.latestVersion)
+      executeMigrations(connection, statements.all())
+      insertVersion(connection, statements.latestVersion)
     }
   }
 
-  private fun getCurrentVersion() = connection
+  private fun getCurrentVersion(connection: Connection) = connection
     .prepareStatement("select version from migration_sets where name = ?")
     .apply { setString(1, name) }
     .executeQuery().use { resultSet ->
@@ -34,18 +35,18 @@ class MigrationService(
       else null
     }
 
-  private fun executeMigrations(statements: List<String>) = statements.forEach { statement ->
+  private fun executeMigrations(connection: Connection, statements: List<String>) = statements.forEach { statement ->
     logger.info("applied: '$statement'")
     connection.createStatement().execute(statement)
   }
 
-  private fun insertVersion(version: MigrationVersion) = connection
+  private fun insertVersion(connection: Connection, version: MigrationVersion) = connection
     .prepareStatement("insert into migration_sets (name, version) values (?, ?)").apply {
       setString(1, name)
       setInt(2, version)
     }.execute()
 
-  private fun updateVersion(version: MigrationVersion) = connection
+  private fun updateVersion(connection: Connection, version: MigrationVersion) = connection
     .prepareStatement("update migration_sets set version = ? where name = ?").apply {
       setInt(1, version)
       setString(2, name)
